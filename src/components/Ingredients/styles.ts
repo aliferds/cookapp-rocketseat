46b6e6cc77261b89f5fccd37b796878750be6862
Alifer DS <aliferduartesilva@gmail.com>
2024-03-05 20:43:50 -0300

    Cook App RocketSeat Aula Ao vivo

---
+++ src/components/Ingredients/styles.ts
@@ -0,0 +1,15 @@
+import { theme } from "@/theme"
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+    paddingBottom: 200,
+    gap: 12,
+  },
+  ingredientsContent: {
+    gap: 12,
+    paddingHorizontal: 32,
+  },
+})
